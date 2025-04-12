96c6e62b55b6f9deefa710eb2b8a7a35283dd6a6
renaldyrs <renaldyrizky83@gmail.com>
2025-04-13 00:19:20 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,14 @@
             <div class="text-center mb-6 flex flex-col items-center">
 
                 <div class="mb-2">
-                    <img src="{{ DB::table('store_profiles')->first()->logo ? asset('storage/' . DB::table('store_profiles')->first()->logo) : asset('images/default-logo.png') }}"
-                        alt="User Avatar" class="w-23 h-23 rounded-full">
+                    @if (DB::table('store_profiles')->first()->logo)
+                        <img src="{{ asset('storage/' . DB::table('store_profiles')->first()->logo) }}"
+                            alt="Logo Toko" class="w-23 h-23 rounded-full">
+                    @else
+                        <p class="mt-2 text-gray-500 dark:text-gray-400">Belum ada logo</p>
+                    
+                    @endif
+                    
 
                 </div>
                 <h1 class="text-2xl font-bold text-gray-800 ">{{ DB::table('store_profiles')->first()->name }}</h1>
