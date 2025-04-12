463eb0a926c988d2c14328a02a3a7ca4af1ed086
renaldyrs <renaldyrizky83@gmail.com>
2025-04-13 00:25:16 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     
 
                 </div>
-                <h1 class="text-2xl font-bold text-gray-800 ">{{ DB::table('store_profiles')->first()->name }}</h1>
+                
             </div>
 
             <form method="POST" action="{{ route('login') }}">
