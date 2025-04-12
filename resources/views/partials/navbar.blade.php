94cce0008051850a702aeacdb4090b6f92ae2f54
renaldyrs <renaldyrizky83@gmail.com>
2025-04-13 03:45:43 +0700

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -93,26 +93,99 @@
                 </div>
                 <div class="px-4 py-4">
                     <nav class="flex flex-col space-y-2">
-                        <a href="#"
-                            class="px-3 py-2 rounded-md text-sm font-medium text-white bg-primary-600 dark:bg-primary-700 flex items-center">
-                            <i class="fas fa-home mr-2"></i> Beranda
-                        </a>
-                        <a href="#"
-                            class="px-3 py-2 rounded-md text-sm font-medium text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center">
-                            <i class="fas fa-box mr-2"></i> Produk
-                        </a>
-                        <a href="#"
-                            class="px-3 py-2 rounded-md text-sm font-medium text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center">
-                            <i class="fas fa-concierge-bell mr-2"></i> Layanan
-                        </a>
-                        <a href="#"
-                            class="px-3 py-2 rounded-md text-sm font-medium text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center">
-                            <i class="fas fa-info-circle mr-2"></i> Tentang
-                        </a>
-                        <a href="#"
-                            class="px-3 py-2 rounded-md text-sm font-medium text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center">
-                            <i class="fas fa-envelope mr-2"></i> Kontak
-                        </a>
+                        <!-- Master Data Dropdown -->
+                    <div class="mb-2">
+                        
+                            <span class="flex items-center">
+                                <i class="fas fa-database mr-3"></i>
+                                <span>Master Data</span>
+                            </span>
+                            
+                        
+                        <ul id="master-data-dropdown" class=" mt-2">
+                            <li class="mb-2">
+                                <a href="{{ route('store-profile.index') }}"
+                                    class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-store mr-3"></i> Toko
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('users.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-users mr-3"></i> Pengguna
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('categories.index') }}"
+                                    class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-tags mr-3"></i> Kategori
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('suppliers.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-truck mr-3"></i> Supplier
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('products.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-boxes mr-3"></i> Produk
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('payment.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-boxes mr-3"></i> Payment
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Transaksi Dropdown -->
+                    <div class="mb-2">
+                        
+                            <span class="flex items-center">
+                                <i class="fas fa-exchange-alt mr-3"></i>
+                                <span>Transaksi</span>
+                            </span>
+                            
+                        <ul id="transaksi-dropdown" class=" mt-2">
+                            <li class="mb-2">
+                                <a href="{{ route('cashier.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-cash-register mr-3"></i> Kasir
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('expenses.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-money-bill-wave mr-3"></i> Pengeluaran
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('returns.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-undo mr-3"></i> Return
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Laporan Dropdown -->
+                    <div class="mb-2">
+                        
+                            <span class="flex items-center">
+                                <i class="fas fa-chart-bar mr-3"></i>
+                                <span>Laporan</span>
+                            </span>
+                            
+                        <ul id="laporan-dropdown" class=" mt-2">
+                            <li class="mb-2">
+                                <a href="{{ route('reports.index') }}" class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-clipboard-list mr-3"></i> Laporan Pesanan
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="{{ route('reports.financial') }}"
+                                    class="block p-3 rounded-lg hover:bg-blue-800">
+                                    <i class="fas fa-chart-line mr-3"></i> Laporan Keuangan
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                     </nav>
                 </div>
             </div>
@@ -157,6 +230,7 @@
             html.classList.add('dark');
         }
     </script>
+    
 </body>
 
 </html>
